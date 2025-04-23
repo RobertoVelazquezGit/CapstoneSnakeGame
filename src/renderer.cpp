f79049e1a62c75f85191083871db46d5a3940f16
RobertoVelazquezGit <vmroberto@hotmail.es>
2025-04-23 21:44:02 +0200

    Renderer constructor, always SDL_Init the first, before creating the pointer to window and renderer

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -9,6 +9,46 @@ Renderer::Renderer(const std::size_t screen_width,
                    const std::size_t grid_width, const std::size_t grid_height)
     : screen_width(screen_width), screen_height(screen_height),
       grid_width(grid_width), grid_height(grid_height),
+      sdl_window(nullptr, SDL_DestroyWindow),
+      sdl_renderer(nullptr, SDL_DestroyRenderer) {
+
+  // Step 1: Initialize SDL
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cerr << "SDL could not initialize.\n";
+    std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
+    return;
+  }
+
+  // Step 2: Create window
+  SDL_Window *raw_window = SDL_CreateWindow(
+      "Snake Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+      screen_width, screen_height, SDL_WINDOW_SHOWN);
+  if (!raw_window) {
+    std::cerr << "Window could not be created.\n";
+    std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
+    return;
+  }
+
+  sdl_window.reset(raw_window);
+
+  // Step 3: Create renderer
+  SDL_Renderer *raw_renderer =
+      SDL_CreateRenderer(raw_window, -1, SDL_RENDERER_ACCELERATED);
+  if (!raw_renderer) {
+    std::cerr << "Renderer could not be created.\n";
+    std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
+    return;
+  }
+
+  sdl_renderer.reset(raw_renderer);
+}
+
+/*
+Renderer::Renderer(const std::size_t screen_width,
+                   const std::size_t screen_height,
+                   const std::size_t grid_width, const std::size_t grid_height)
+    : screen_width(screen_width), screen_height(screen_height),
+      grid_width(grid_width), grid_height(grid_height),
       sdl_window(SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED,
                                   SDL_WINDOWPOS_CENTERED, screen_width,
                                   screen_height, SDL_WINDOW_SHOWN),
@@ -34,6 +74,7 @@ Renderer::Renderer(const std::size_t screen_width,
     std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
   }
 }
+*/
 
 // Destructor
 Renderer::~Renderer() { SDL_Quit(); }
@@ -105,19 +146,16 @@ Renderer &Renderer::operator=(const Renderer &source) {
 
 // Movement constructor
 Renderer::Renderer(Renderer &&other)
-    : screen_width(other.screen_width),
-      screen_height(other.screen_height),
-      grid_width(other.grid_width),
-      grid_height(other.grid_height),
+    : screen_width(other.screen_width), screen_height(other.screen_height),
+      grid_width(other.grid_width), grid_height(other.grid_height),
       sdl_window(std::move(other.sdl_window)),
       sdl_renderer(std::move(other.sdl_renderer)) {}
 
-// Movement assignment operator      
-Renderer& Renderer::operator = (Renderer &&other){
-  if(&other == this){
+// Movement assignment operator
+Renderer &Renderer::operator=(Renderer &&other) {
+  if (&other == this) {
     return *this;
-  }    
-  else{
+  } else {
     this->screen_width = other.screen_width;
     this->screen_height = other.screen_height;
     this->grid_width = other.grid_width;
@@ -137,11 +175,13 @@ void Renderer::Render(Snake const snake, SDL_Point const &food,
   block.h = screen_height / grid_height;
 
   // Clear screen
-  SDL_SetRenderDrawColor(sdl_renderer.get(), 0x1E, 0x1E, 0x1E, 0xFF);  // Dark grey
+  SDL_SetRenderDrawColor(sdl_renderer.get(), 0x1E, 0x1E, 0x1E,
+                         0xFF); // Dark grey
   SDL_RenderClear(sdl_renderer.get());
 
   // Render food
-  SDL_SetRenderDrawColor(sdl_renderer.get(), 0xFF, 0xCC, 0x00, 0xFF);  // Bright yellow
+  SDL_SetRenderDrawColor(sdl_renderer.get(), 0xFF, 0xCC, 0x00,
+                         0xFF); // Bright yellow
   block.x = food.x * block.w;
   block.y = food.y * block.h;
   SDL_RenderFillRect(sdl_renderer.get(), &block);
@@ -150,11 +190,11 @@ void Renderer::Render(Snake const snake, SDL_Point const &food,
   bool snakecolorflag = snakecolor.isFlagColorSet();
 
   // Render snake's body
-  if(snakecolorflag){
-    SDL_SetRenderDrawColor(sdl_renderer.get(), 0x22, 0x8B, 0x22, 0xFF);  // Dark green
-  }
-  else{
-    SDL_SetRenderDrawColor(sdl_renderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);  // White
+  if (snakecolorflag) {
+    SDL_SetRenderDrawColor(sdl_renderer.get(), 0x22, 0x8B, 0x22,
+                           0xFF); // Dark green
+  } else {
+    SDL_SetRenderDrawColor(sdl_renderer.get(), 0xFF, 0xFF, 0xFF, 0xFF); // White
   }
   for (SDL_Point const &point : snake.body) {
     block.x = point.x * block.w;
@@ -166,19 +206,21 @@ void Renderer::Render(Snake const snake, SDL_Point const &food,
   block.x = static_cast<int>(snake.head_x) * block.w;
   block.y = static_cast<int>(snake.head_y) * block.h;
   if (snake.alive) {
-    if(snakecolorflag){
-      SDL_SetRenderDrawColor(sdl_renderer.get(), 0x00, 0xFF, 0x00, 0xFF);  // Green
+    if (snakecolorflag) {
+      SDL_SetRenderDrawColor(sdl_renderer.get(), 0x00, 0xFF, 0x00,
+                             0xFF); // Green
+    } else {
+      SDL_SetRenderDrawColor(sdl_renderer.get(), 0x00, 0x7A, 0xCC,
+                             0xFF); // Blue
     }
-    else{
-      SDL_SetRenderDrawColor(sdl_renderer.get(), 0x00, 0x7A, 0xCC, 0xFF);  // Blue
-    }    
   } else {
-    SDL_SetRenderDrawColor(sdl_renderer.get(), 0xBF, 0x00, 0xFF, 0xFF);  // Purple
+    SDL_SetRenderDrawColor(sdl_renderer.get(), 0xBF, 0x00, 0xFF,
+                           0xFF); // Purple
   }
   SDL_RenderFillRect(sdl_renderer.get(), &block);
 
   // Render line barrier
-  SDL_SetRenderDrawColor(sdl_renderer.get(), 255, 0, 0, 255);  // Red
+  SDL_SetRenderDrawColor(sdl_renderer.get(), 255, 0, 0, 255); // Red
   for (const SDL_Point &barrierPoint : linebarrier.getBarrierPoints()) {
     block.x = barrierPoint.x * block.w;
     block.y = barrierPoint.y * block.h;
@@ -186,7 +228,7 @@ void Renderer::Render(Snake const snake, SDL_Point const &food,
   }
 
   // Render square barrier
-  SDL_SetRenderDrawColor(sdl_renderer.get(), 255, 0, 0, 255);  // Red
+  SDL_SetRenderDrawColor(sdl_renderer.get(), 255, 0, 0, 255); // Red
   for (const SDL_Point &barrierPoint : squarebarrier.getBarrierPoints()) {
     block.x = barrierPoint.x * block.w;
     block.y = barrierPoint.y * block.h;
@@ -198,7 +240,7 @@ void Renderer::Render(Snake const snake, SDL_Point const &food,
 }
 
 void Renderer::UpdateWindowTitle(int score, int fps, bool gameisover) {
-  std::string title{"Snake Score: " + std::to_string(score) +
-                    " FPS: " + std::to_string(fps) + (gameisover ? " GAME IS OVER" : "")};
+  std::string title{"Snake Score: " + std::to_string(score) + " FPS: " +
+                    std::to_string(fps) + (gameisover ? " GAME IS OVER" : "")};
   SDL_SetWindowTitle(sdl_window.get(), title.c_str());
 }
